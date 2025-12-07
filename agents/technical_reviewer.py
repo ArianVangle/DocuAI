4ba07b129afe3813a11bf1c3c93f4ae7f8211466
ArianVangle <ariansemenoff@yandex.ru>
2025-12-07 14:13:57 +0300

    not RAG

--- agents/technical_reviewer.py
+++ agents/technical_reviewer.py
@@ -3,37 +3,18 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import os
 
-def answer_technical_question(document_text: str, question: str, vectorstore=None) -> str:
-    if vectorstore is not None:
-    # RAG-режим
-        from langchain.chains import RetrievalQA
-        from langchain_gigachat import GigaChat
-        import os
-        
-        llm = GigaChat(
-            credentials=os.getenv("API_KEY"),
-            model="GigaChat",
-            verify_ssl_certs=False
-        )
-        
-        qa_chain = RetrievalQA.from_chain_type(
-            llm=llm,
-            chain_type="stuff",
-            retriever=vectorstore.as_retriever(search_kwargs={"k": 3})
-        )
-        return qa_chain.invoke({"query": question})["result"]
+def answer_technical_question(document_text: str, question: str) -> str:
     
-    else:
-        llm = GigaChat(
-            credentials=os.getenv("API_KEY"),
-            model="GigaChat",
-            verify_ssl_certs=False
-        )
-
-        prompt = ChatPromptTemplate.from_messages([
-            ("system", "Ты — технический эксперт. Отвечай ТОЛЬКО на основе документа. Если информации нет — скажи об этом.\n\nДокумент:\n{document}"),
-            ("human", "{question}")
-        ])
-
-        chain = prompt | llm | StrOutputParser()
-        return chain.invoke({"document": document_text, "question": question})
+    llm = GigaChat(
+        credentials=os.getenv("API_KEY"),
+        model="GigaChat",
+        verify_ssl_certs=False
+    )
+    
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Ты — технический эксперт. Отвечай ТОЛЬКО на основе документа. Если информации нет — скажи об этом.\n\nДокумент:\n{document}"),
+        ("human", "{question}")
+    ])
+    
+    chain = prompt | llm | StrOutputParser()
+    return chain.invoke({"document": document_text, "question": question})
